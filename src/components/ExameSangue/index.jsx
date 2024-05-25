469f20530ba5f68886fb7fae1905a43c937fc266
unknown <pedrolimasteam@gmail.com>
2024-05-25 00:55:56 -0300

    video

--- src/components/ExameSangue/index.jsx
+++ src/components/ExameSangue/index.jsx
@@ -11,7 +11,7 @@ function ExameSangue() {
             
 
                 <div className="flex justify-center py-10">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/eEl2vRZ2tL8?si=e7ZcPeQP7_P8zLB3" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen className="border-8 border-cyan-300"></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/HgNPdXTCXKQ?si=QBXFjD_4FhrbjS6m" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
             </div>
 
