9ee5b6acbf445fff82c08f3aa5364953086f5456
unknown <pedrolimasteam@gmail.com>
2024-05-25 00:57:17 -0300

    video

--- src/components/RaioX/index.jsx
+++ src/components/RaioX/index.jsx
@@ -11,7 +11,7 @@ function RaioX() {
             
 
                 <div className="flex justify-center py-10">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/eEl2vRZ2tL8?si=e7ZcPeQP7_P8zLB3" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen className="border-8 border-cyan-300"></iframe>
+                    
                 </div>
             </div>
 
